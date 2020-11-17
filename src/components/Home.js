a78a897379ce1123d0b552a0b3e0429bb14a2722
Kamil Dziekan <dziekan30@gmail.com>
2020-11-17 11:49:02 -0400

    prep for deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -100,49 +100,6 @@ export default class Home extends Component {
         </Container>
         <Counter />
       </div>
-      // <div>
-      //     <div className="container">
-      //       <h1>Count My Tip</h1>
-      //       <div className="my-tip">
-      //         <form onSubmit={this.genTip}>
-      //           <label className="col-12 label" >
-      //             Bill Total:
-      //             <i className="fas fa-dollar-sign "></i>
-      //             <MDBInput placeholder="Large" type="text" value={this.state.total} onChange={this.handleTotal} />
-      //           </label>
-      //           {/* <label className="col-12 label" >
-      //             Bill Total:
-      //             <i className="fas fa-dollar-sign "></i>
-      //             <MDBInput type="text" value={this.state.total} onChange={this.handleTotal} />
-      //           </label> */}
-      //           <label className="col-12 label">
-      //             Service: %
-      //             <MDBInput type="text" value={this.state.service} onChange={this.handleService} />
-      //           </label>
-      //           <label className="col-12 label">
-      //             Party Size:
-      //             <MDBInput type="number" value={this.state.partySize} onChange={this.handleParty} />
-      //           </label>
-      //           {/* <label placeholder="Your value" type="text" id="form5" class="form-control">hhhhhhh.</label> */}
-      //           <input className="btn btn-secondary col-12 label" onClick={this.handleOpenModal} type="submit" />
-      //         </form>
-      // <div >
-      //   <ReactModal
-      //     isOpen={this.state.showModal}
-      //     contentLabel="Minimal Modal Example"
-      //     className="Modal"
-      //   >
-      //     <button className="btn btn-secondary modal-close" type="button" onClick={this.handleCloseModal}>Close</button>
-      //     <div>
-      //       <h3 className="tip">Tip: ${this.state.totalTip}</h3>
-      //       <h3 className="tip">Perperson: ${this.state.tipPerPerson}</h3>
-      //     </div>
-      //   </ReactModal>
-      // </div>
-      //       </div>
-      //     </div>
-      //   <Counter />
-      // </div>
     );
 
   }
